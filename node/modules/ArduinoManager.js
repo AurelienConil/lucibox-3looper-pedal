62a2888b29d2b14f085ebc6dcb0cba76f851a2d1
Aurelien C <conil.a@hotmail.fr>
2025-07-24 15:29:39 +0200

    increase serial port detection

--- node/modules/ArduinoManager.js
+++ node/modules/ArduinoManager.js
@@ -44,7 +44,7 @@ class ArduinoManager {
       const timeout = setTimeout(() => {
         cleanup();
         resolve(false);
-      }, 2000); // 3 secondes timeout
+      }, 2500); // 3 secondes timeout
       
       let testPort;
       
